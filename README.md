7fca3e3949ea2f8b14329fa434c13448b40902b1
aipa <gundamgundam777@gmail.com>
2016-11-26 00:21:28 +0900

    [modify] pikadayと連携

--- README.md
+++ README.md
@@ -52,11 +52,11 @@ import {
 
 todo
 
-* Enter keyを無効にしたい
-* from ~ toのようなフォームの場合、矛盾が起きないようにカスタムバリデーションを用意したい
+* [Enter keyを無効にしたい](#enter-keyを無効にしたい)
+* [from ~ toのようなフォームの場合、矛盾が起きないようにカスタムバリデーションを用意したい](#他の入力情報と比較してvalidateしたい)
   * 09:00 ~ 10:00はおｋ
   * 10:00 = 09:00はダメみたいな
-* datepickerと組み合わせたinput formがほしい
+* [datepickerと組み合わせたinput formがほしい](#datepickerと組み合わせたinput formがほしい)
   * http://dbushell.github.io/Pikaday/
   * pikadayってやつがいまのところよさそう
 
@@ -94,3 +94,51 @@ this.myForm.controls['test_datetime_validate_end'].valueChanges.subscribe(
   }
 );
 ```
+
+## Pikadayを使う
+
+3rd party libraryの使い方になるとか。あと`moment.js`も依存しているらしいので、これもいれないといけない。
+
+```sh
+$ npm install pikaday moment --save-dev
+```
+
+- angular-cli.json
+```
+"app": [{ 
+      "scripts": [
+        "../node_modules/pikaday/pikaday.js"
+      ]
+}]
+```
+
+これでおｋ
+
+
+あとは使いたいところで
+
+```
+// 宣言？
+declare var Pikaday: any;
+
+var picker = new Pikaday({
+  // ElementRefからbindするhtmlタグをセット
+  field: this.elementRef.nativeElement.querySelector('#test_datetime_picker'),
+  // momentを使ったフォーマット変更
+  format: 'YYYY/MM/DD',
+  // 日本語化
+  i18n: {
+    previousMonth : '先月',
+    nextMonth     : '来月',
+    months        : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+    weekdays      : ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+    weekdaysShort : ['日','月','火','水','木','金','土']
+  },
+  onSelect: (date) => {
+    // コールバックで値をセット。じゃないと変更が検知できないし、値がセットされない。
+    this.myForm.controls['test_datetime_picker'].setValue(picker.toString());
+  }
+});
+```
+
+って、やれば使えるようになりました。日本語化も対応。
--- angular2-form-demo/src/app/form/form.component.ts
+++ angular2-form-demo/src/app/form/form.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { formatDate, TimeMath } from '../util/functions';
+
+declare var Pikaday: any;
 
 function skuValidator(control: FormControl): { [s: string]: boolean } {
   if (control.value) {
@@ -19,8 +21,10 @@ function skuValidator(control: FormControl): { [s: string]: boolean } {
 export class FormComponent implements OnInit {
   myForm: FormGroup;
   datetime: string[];
+  private elementRef: ElementRef;
 
-  constructor(formBuilder: FormBuilder) {
+  constructor(formBuilder: FormBuilder, elementRef: ElementRef) {
+    this.elementRef = elementRef;
     this.myForm = formBuilder.group({
       'sku': [, Validators.compose([
         Validators.required,
@@ -33,6 +37,9 @@ export class FormComponent implements OnInit {
         Validators.required
       ])],
       'test_datetime_validate_end': ['12:30', Validators.compose([
+        Validators.required
+      ])],
+      'test_datetime_picker': [, Validators.compose([
         Validators.required
       ])]
     });
@@ -64,10 +71,30 @@ export class FormComponent implements OnInit {
         }
       }
     );
+
+    this.myForm.controls['test_datetime_picker'].valueChanges.subscribe(
+      (value: string) => {
+        console.log(value);
+      }
+    );
   }
 
   ngOnInit() {
     this.setDatetime();
+    var picker = new Pikaday({
+      field: this.elementRef.nativeElement.querySelector('#test_datetime_picker'),
+      format: 'YYYY/MM/DD',
+      i18n: {
+        previousMonth : '先月',
+        nextMonth     : '来月',
+        months        : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+        weekdays      : ['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+        weekdaysShort : ['日','月','火','水','木','金','土']
+      },
+      onSelect: (date) => {
+        this.myForm.controls['test_datetime_picker'].setValue(picker.toString());
+      }
+    });
   }
 
   setDatetime(): void {
